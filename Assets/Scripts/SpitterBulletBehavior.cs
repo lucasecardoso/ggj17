828417f51a3d9bf2397c4ffdf10d00557169ff35
Lucas Cardoso <lucasecardoso@gmail.com>
2017-01-21 17:09:41 -0300

    agrega prefabs de balas y enemigo escupidor

---
+++ Assets/Scripts/SpitterBulletBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitterBulletBehavior : MonoBehaviour {
+
+	Rigidbody2D rb;
+	private float timeToLive = 5f;
+	private float counter;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.velocity.y < 0f) {
+			//Aca hay que animar para que tenga el inverso de la bala o lo que fuere
+		}
+
+		counter += 1f * Time.deltaTime;
+
+		if (counter > timeToLive) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SpitterEnemyBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitterEnemyBehavior : MonoBehaviour {
+
+	public float bulletThrust = 1000f;
+
+	public GameObject bullet;
+
+	private float timer = 0f;
+	private bool canShoot = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		timer += 1f * Time.deltaTime;
+
+		if (timer > 0.8f && canShoot) {
+			Shoot ();
+			canShoot = false;
+		}
+	}
+
+	private void Shoot () {
+		GameObject b = (GameObject)Instantiate (bullet, transform.position, transform.rotation);
+		b.GetComponent<Rigidbody2D> ().AddForce (b.transform.up * bulletThrust);
+	}
+}
